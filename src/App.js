105069a952859a33ff8aca22c9117437effe953d
GadzhievElnar <87620178+GadzhievElnar@users.noreply.github.com>
2021-08-02 23:48:56 +0300

    19 route, browser-router, маршрутизация

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import Header from './components/Header/Header'
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <NavBar />
-      <div className='app-wrapper-content'>
-        <Profile />
-        <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <NavBar />
+        <div className='app-wrapper-content'>
+          <Route component={Profile} path='/Profile'/>
+          <Route component={Dialogs} path='/Dialogs'/>       
+          <Route component={News} path='/News' />
+          <Route component={Music} path='/Music' />
+          <Route component={Settings} path='/Settings' /> 
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,13 @@
+import { returnStatement } from '@babel/types';
+import React from 'react';
+import styles from './Music.module.css';
+
+const Music = (props) => {
+    return(
+        <div>
+            Music
+        </div>
+    );
+}
+
+export default Music;
