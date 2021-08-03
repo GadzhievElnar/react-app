8a8db1b480f3c2279f2970482e2b1cf78b8a4e37
GadzhievElnar <87620178+GadzhievElnar@users.noreply.github.com>
2021-08-03 23:40:00 +0300

    22 Верстаем страницу Диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,35 @@
-//import React from 'react';
+import React from 'react';
 import styles from './Dialogs.module.css';
 
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog + ' ' + styles.active}>
+                    Tania
+                </div>
+
+                <div className={styles.dialog}>
+                    Artur
+                </div>   
+
+                <div className={styles.dialog}>
+                    Zhenia
+                </div>   
+
+                <div className={styles.dialog}>
+                    Stas
+                </div>                                
+            </div>
+
+            <div className={styles.messages}>
+                <div className={styles.message}>Hi</div>
+                <div className={styles.message}>How are you?</div>
+                <div className={styles.message}></div>
+                <div className={styles.message}></div>
+                <div className={styles.message}></div>
+            </div>            
         </div>
     );
 }
