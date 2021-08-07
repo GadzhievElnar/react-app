6f95b8d3279eaa5c8498f3a97f302fe0a8520f4d
GadzhievElnar <87620178+GadzhievElnar@users.noreply.github.com>
2021-08-07 23:42:04 +0300

    27 структура проекта, ещё раз про ui - bll

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import styles from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let postsData = [
-        {id: 1, message: 'Hi, how are you?', likesCount: 15},
-        {id: 2, message: 'It`s my first post', likesCount: 20},
-        {id: 2, message: 'Array method MAP', likesCount: 10}         
-    ];
 
-    let postsElements = postsData.map( post => <Post id={post.id} message={post.message} likeCount={post.likesCount} /> );
+
+    let postsElements = props.posts.map( post => <Post id={post.id} message={post.message} likeCount={post.likesCount} /> );
 
     return(
     <div className={styles.postsBlock}>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,10 +4,17 @@ import styles from './Profile.module.css';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
+
+    let postsData = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 15},
+        {id: 2, message: 'It`s my first post', likesCount: 20},
+        {id: 2, message: 'Array method MAP', likesCount: 10}         
+    ];
+
     return (
         <div className={styles.content}>
             <ProfileInfo />
-            <MyPosts />
+            <MyPosts posts={postsData}/>
         </div>
     );
 }
