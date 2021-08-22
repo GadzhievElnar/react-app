859e901e1ccc94bf9d2a5b5c6a55024adafb0b77
GadzhievElnar <87620178+GadzhievElnar@users.noreply.github.com>
2021-08-22 23:00:42 +0300

    52 side effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,18 +3,22 @@ import React from 'react';
 import User from './User/User';
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        debugger;
-        axios
-        .get('https://social-network.samuraijs.com/api/1.0/users')
-        .then(response => (props.setUsers(response.data.items)));
-        debugger;
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            debugger;
+            axios
+            .get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => (props.setUsers(response.data.items)));
+            debugger;
+        }
     }
+
 
     let usersElements = props.users.map(user => { return (<User user={user} follow={props.follow} unfollow={props.unfollow} />) });
 
     return (
         <div>
+            <div><button onClick={getUsers}>GetUsers</button></div>
             {usersElements}
         </div>
     )
