df73a243e99f899627164c093857ff0b3e5cc336
pavel_m <pavel_m@NET>
2021-08-31 17:39:12 +0300

    68.1 Login

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/Header/HeaderContainer'
 import NavBar from './components/NavBar/NavBar';
-import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
 import { Route } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
@@ -11,6 +9,7 @@ import Settings from './components/Settings/Settings';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import Login from './components/Login/Login';
 
 const App = (props) => {  
   return (
@@ -25,6 +24,8 @@ const App = (props) => {
           <Route component={Music} path='/Music' />
           <Route component={Settings} path='/Settings' /> 
           <Route render={() => <UsersContainer />} path='/Users' /> 
+
+          <Route component={Login} path='/Login' /> 
         </div>
       </div>
   );
